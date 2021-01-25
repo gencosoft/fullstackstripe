deba3bbc2916f86a5a6b23f090079c05d71f8441
MEHMET RAŞİD GENCOSMANOĞLU <mehmet.gencosmanoglu@jcr.local>
2021-01-25 12:05:45 +0300

    Subscription page ui improvement

--- stripe-vue-express/ui/src/components/Subscription.vue
+++ stripe-vue-express/ui/src/components/Subscription.vue
@@ -3,23 +3,29 @@
     <v-row align="center" justify="center">
       <v-col>
         <v-hover v-slot="{ hover }">
-          <v-card :elevation="hover ? 16 : 2" width="500">
+          <v-card  :elevation="hover ? 16 : 2" width="300">
+            <v-img
+              height="200px"
+              src="https://images.unsplash.com/photo-1592910147752-5e0bc5f04715?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+            >
+            </v-img>
+
             <v-card-title class="teal--text darken-1">
-              Basic $5.00
+              Basic - $5.00
             </v-card-title>
-            <v-card-subtitle class="teal--text darken-1">
-              <p class="teal--text darken-1">Per Month</p>
-              <p class="teal--text darken-1">Billed Monthly</p>
+            <v-card-subtitle>
+              <p class="teal--text darken-1">Per Month (Billed Monthly)</p>
             </v-card-subtitle>
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn
                 :loading="isPlan1Loading"
                 @click="selectPlan(priceIdPlan1, 1)"
-                dark
-                x-large
-                color="teal darken-1"
-                >Select</v-btn
+                block    
+                dark 
+                rounded    
+                color="teal darken-1"    
+                >Subscribe</v-btn
               >
             </v-card-actions>
           </v-card>
@@ -27,23 +33,30 @@
       </v-col>
       <v-col>
         <v-hover v-slot="{ hover }">
-          <v-card :elevation="hover ? 16 : 2" width="500">
+          <v-card :elevation="hover ? 16 : 2" width="300">
+            <v-img
+              height="200px"
+              src="https://images.unsplash.com/photo-1516048015710-7a3b4c86be43?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+            >
+            </v-img>
+
             <v-card-title class="teal--text darken-1">
-              Premium $50.00
+              Premium - $50.00
             </v-card-title>
             <v-card-subtitle class="teal--text darken-1">
-              <p class="teal--text darken-1">Per Month</p>
-              <p class="teal--text darken-1">Billed Monthly</p>
+              <p class="teal--text darken-1">Per Month (Billed Monthly)</p>
             </v-card-subtitle>
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn
                 @click="selectPlan(priceIdPlan2, 2)"
+                block
                 dark
+                rounded    
+                color="teal darken-1" 
                 :loading="isPlan2Loading"
-                x-large
-                color="teal darken-1"
-                >Select</v-btn
+                  
+                >Subscribe</v-btn
               >
             </v-card-actions>
           </v-card></v-hover
@@ -99,63 +112,3 @@ export default {
   },
 };
 </script>
-
-<style>
-/* Variables */
-* {
-  box-sizing: border-box;
-}
-
-body {
-  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-  font-size: 16px;
-  -webkit-font-smoothing: antialiased;
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  height: 100vh;
-  width: 100vw;
-}
-
-form {
-  width: 30vw;
-  min-width: 500px;
-  align-self: center;
-  box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
-    0px 2px 5px 0px rgba(50, 50, 93, 0.1), 0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
-  border-radius: 7px;
-  padding: 40px;
-}
-
-input {
-  border-radius: 6px;
-  margin-bottom: 6px;
-  padding: 12px;
-  border: 1px solid rgba(50, 50, 93, 0.1);
-  height: 44px;
-  font-size: 16px;
-  width: 100%;
-  background: white;
-}
-
-#card-error {
-  color: rgb(105, 115, 134);
-  text-align: left;
-  font-size: 13px;
-  line-height: 17px;
-  margin-top: 12px;
-}
-
-#card-element {
-  border-radius: 4px 4px 0 0;
-  padding: 12px;
-  border: 1px solid rgba(50, 50, 93, 0.1);
-  height: 44px;
-  width: 100%;
-  background: white;
-}
-
-#payment-request-button {
-  margin-bottom: 32px;
-}
-</style>
